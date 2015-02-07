f904680721b9cbc38873c6b643736ba156fa6a65
Owen <owenrham@gmail.com>
2015-02-07 16:58:06 -0500

    Create login page and authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,41 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    if session[:teacher_id]
+      Teacher.where(id: session[:teacher_id]).first
+    elsif session[:student_id]
+      Student.where(id: session[:student_id]).first
+    elsif session[:parent_id]
+      Parent.where(id: session[:parent_id]).first
+    end
+  end
+
+  helper_method :current_user
+
+  def logged_in?
+    unless current_user
+      redirect_to root_path
+    end
+  end
+
+  def redirect_path
+    if session[:teacher_id]
+      redirect_to teachers_path
+    elsif session[:student_id]
+      redirect_to students_path
+    elsif session[:parent_id]
+      redirect_to parents_path
+    end
+  end
+
+  def permission?
+    unless session[:teacher_id]
+      redirect_to root_path
+    end
+  end
+
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,43 @@
+class SessionsController < ApplicationController
+
+  def create
+    if request.post?
+      if t = Teacher.find_by_email(params[:email])
+        if t && t.authenticate(params[:password])
+          session[:teacher_id] = t.id
+          redirect_to teachers_path, notice: "You've logged in."
+          return
+        else
+          redirect_to root_path, notice: "Wrong email or password."
+          return
+        end
+      elsif s = Student.find_by_email(params[:email])
+        if s && s.authenticate(params[:password])
+          session[:student_id] = s.id
+          redirect_to students_path, notice: "You've logged in."
+          return
+        else
+          redirect_to root_path, notice: "Wrong email or password."
+          return
+        end
+      elsif p = Parent.find_by_email(params[:email])
+        if p && p.authenticate(params[:password])
+          session[:parent_id] = p.id
+          redirect_to parents_path, notice: "You've logged in."
+          return
+        else
+          redirect_to root_path, notice: "Wrong email or password."
+          return
+        end
+      end
+    end
+    redirect_to root_path, notice: "Wrong email or password."
+  end
+
+  def destroy
+    session[:teacher_id] = nil
+    session[:student_id] = nil
+    session[:parent_id] = nil
+    redirect_to root_path, notice: "You've logged out."
+  end
+end
